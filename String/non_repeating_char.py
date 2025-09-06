fede95a00314621a5ad7569579bed35f106a8990
Narendra <narendragatram8533@gmail.com>
2025-09-06 22:04:59 +0530

    solved the first  non repeating char in a stirng

---
+++ String/non_repeating_char.py
@@ -0,0 +1,26 @@
+'''
+    Problem: First non-repeating character in a string    
+    https://www.geeksforgeeks.org/problems/non-repeating-character-1587115620/1?page=1&category=Strings&sortBy=submissions
+    Category: Strings       
+    Difficulty: Easy
+'''
+
+
+class Solution:
+    def nonRepeatingChar(self,s):
+        #code here
+        d = {}
+        
+        for i in s:
+            if i in d.keys():
+                d[i] += 1
+            else:
+                d[i] = 1
+        
+        for i in s:
+            if d[i] == 1:
+                return i
+        
+        return '$'
+    
+    
