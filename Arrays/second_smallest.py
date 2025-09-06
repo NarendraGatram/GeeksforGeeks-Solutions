4ba7c3631a85689c38b9f0a54dd59fff8cd4ff64
Narendra <narendragatram8533@gmail.com>
2025-09-06 19:09:15 +0530

    solved the second smallest problem

---
+++ Arrays/second_smallest.py
@@ -0,0 +1,30 @@
+'''Find the second smallest element in an array. If no such element exists, return -1.
+    Problem: Second Smallest Element
+    Category: Arrays
+    Difficulty: Easy
+'''
+
+
+
+def second_smallest(arr):
+    if len(arr) < 2:
+        return -1
+
+    mini = arr[0]
+    second_smallest = float('inf')
+
+    for i in range(1, len(arr)):
+        if arr[i] < mini:
+            second_smallest = mini
+            mini = arr[i]
+        elif arr[i] > mini and arr[i] < second_smallest:
+            second_smallest = arr[i]
+
+    if second_smallest != float('inf'):
+        return second_smallest
+    else:
+        return -1
+    
+# Example usage:
+arr = eval(input("Enter the array: "))
+print("The second smallest element is:", second_smallest(arr))
