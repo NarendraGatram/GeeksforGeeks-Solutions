c5eaab71ccb7379402d091d483f2f4fc0ce7d813
Narendra <narendragatram8533@gmail.com>
2025-09-09 10:16:30 +0530

    solved the Largest element in the list

---
+++ Arrays/largest_element.py
@@ -0,0 +1,16 @@
+'''
+    problem : Largest element in the list 
+    Difficulty : Easy
+'''
+def largest(lt,n):
+    maximum = lt[0]
+    for i in range(1,n):
+        if lt[i]>maximum:
+            maximum = lt[i]
+    return maximum
+
+lt = input('Enter list sepaated by space : ')
+lt = list(lt.split())
+n = len(lt)
+
+print(f'Largest element in the list : {largest(lt,n)}')
